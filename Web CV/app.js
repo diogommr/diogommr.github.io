89345a398dc0c9d2f09cd95092f9f483f46baba9
Diogo Ribeiro <diogommr@gmail.com>
2017-11-02 18:42:51 +0000

    web cv

---
+++ Web CV/app.js
@@ -0,0 +1,25 @@
+
+var app = angular.module('cvApp', []);
+
+app.controller('mainController', function($scope, $log, $http, $cacheFactory) {
+
+	$http.get("cv_front-end_en.json", { cache: true })
+		.then(function(response) {
+			var rawData = JSON.stringify(response.data);
+			$scope.sections = JSON.parse(rawData);
+			//$log.info( $scope.sections ); // Array
+
+			$scope.skills = Object.keys($scope.sections[5].skills);
+
+			for(var i = 0; i < $scope.sections.length; i++){
+				$log.info( $scope.sections[i].sectionName );
+				if( $scope.sections[i].sectionName === "Personal Info" ){
+					$log.log("Personal");
+				}
+			}
+
+		});
+
+
+
+});
